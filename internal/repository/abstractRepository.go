75b033889a99d8b5ae135b28e55ca381f7c6393c
Kirill Sofranchuk <kirill.sofranchuk@mego.travel>
2025-04-20 20:43:52 +0300

    Add communication by channels and graceful shutdown

--- internal/repository/abstractRepository.go
+++ internal/repository/abstractRepository.go
@@ -4,4 +4,7 @@ import "Notes/internal/model"
 
 type AbstractRepository interface {
 	SaveEntity(entity model.BusinessEntity)
+	GetUsers() []*model.User
+	GetNotes() []*model.Note
+	GetFolders() []*model.Folder
 }
